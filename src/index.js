1059669c43e5698ef90256527d0e1bf5625e5500
Saharsh Yeruva <github@saharsh.tech>
2020-07-09 16:49:38 -0400

    🗑testing: cleaning up code used for testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './styles/main.scss';
-
-const greeting = 'Hello World';
-console.log(greeting);
-
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
-
-getData('https://jsonplaceholder.typicode.com/posts');
