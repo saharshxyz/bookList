93d4c30eff12c77052b33f9f8ad482318339641e
Saharsh Yeruva <github@saharsh.tech>
2020-07-09 16:46:06 -0400

    💩testing: testing babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './styles/main.scss';
 
-console.log('testing');
+const greeting = 'Hello World';
+console.log(greeting);
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+};
+
+getData('https://jsonplaceholder.typicode.com/posts');
