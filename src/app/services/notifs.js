8abe41e848d117d042b25ca47ce0e967ab904800
Saharsh Yeruva <github@saharsh.tech>
2020-07-25 18:37:16 -0400

    ✨ add notifications

--- src/app/services/notifs.js
+++ src/app/services/notifs.js
@@ -1,6 +1,20 @@
 import { isISBN } from '../utils/confirmISBN';
 
-const displayNotif = (message, color) => {};
+const displayNotif = (message, color, location) => {
+  const notif = document.createElement('div');
+  notif.classList.add("notification");
+  notif.classList.add(color);
+  notif.innerHTML(message);
+
+if (location == "form") {
+const form = document.getElementById('form')
+form.appendChild(notif);
+} else if (location == "table") {
+const table = document.getElementById('book-table');
+table.appendChild(notif);
+}
+};
+
 
 if (isISBN(ISBN)) {
 } else {
