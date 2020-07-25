04c3a89bba4a5f6e8702a4f1d5ed43bea72a2e07
Linter <github@saharsh.tech>
2020-07-25 22:42:33 +0000

    🚨Prettier: Fix code style issues

--- src/app/services/notifs.js
+++ src/app/services/notifs.js
@@ -2,24 +2,27 @@ import { isISBN } from '../utils/confirmISBN';
 
 const displayNotif = (message, color, location) => {
   const notif = document.createElement('div');
-  notif.classList.add("notification");
+  notif.classList.add('notification');
   notif.classList.add(color);
   notif.innerHTML(message);
 
-if (location == "form") {
-const form = document.getElementById('form')
-form.appendChild(notif);
-} else if (location == "table") {
-const table = document.getElementById('book-table');
-table.appendChild(notif);
-}
+  if (location == 'form') {
+    const form = document.getElementById('form');
+    form.appendChild(notif);
+  } else if (location == 'table') {
+    const table = document.getElementById('book-table');
+    table.appendChild(notif);
+  }
 
-setTimeout(() => {
-  notif.remove();
-}, 5000);
+  setTimeout(() => {
+    notif.remove();
+  }, 5000);
 };
 
-
 if (isISBN(ISBN) == false) {
-  displayNotif("Invalid ISBN. Please make sure it's in ISBN-13 format.", "is-danger", "form")
+  displayNotif(
+    "Invalid ISBN. Please make sure it's in ISBN-13 format.",
+    'is-danger',
+    'form'
+  );
 }
