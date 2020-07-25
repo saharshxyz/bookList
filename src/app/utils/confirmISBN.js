69749e86cb2a6036cf51eeb6084426990e93c775
Linter <github@saharsh.tech>
2020-07-25 20:10:06 +0000

    🚨Prettier: Fix code style issues

--- src/app/utils/confirmISBN.js
+++ src/app/utils/confirmISBN.js
@@ -1,9 +1,8 @@
 export const isISBN = (ISBN) => {
-if (ISBN.toString().length === 13) {
+  if (ISBN.toString().length === 13) {
     return true;
-  } 
-    return false;
-  
-}
+  }
+  return false;
+};
 
 export default isISBN;
