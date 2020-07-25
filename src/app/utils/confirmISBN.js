f124bbfc6f282a3471bc02cae5233c8dd60f0451
Saharsh Yeruva <hello@saharsh.tech>
2020-07-25 15:59:52 -0400

    ♻️ chore: simplify ISBN checking

--- src/app/utils/confirmISBN.js
+++ src/app/utils/confirmISBN.js
@@ -1,8 +1,8 @@
 export const isISBN = (ISBN) => {
-if (parseInt(ISBN, 10) === NaN) {
-  return false
-  } else if (ISBN.toString().length === 13) {
+if (ISBN.toString().length === 13) {
     return true;
+  } else {
+    return false;
   }
 }
 
