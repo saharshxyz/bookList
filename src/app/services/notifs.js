afcffcefa88830166eb7e61e88405a1f6411de34
Saharsh Yeruva <hello@saharsh.tech>
2020-07-25 21:46:03 -0400

    💩broken: trying to add notification

--- src/app/services/notifs.js
+++ src/app/services/notifs.js
@@ -1,28 +1,21 @@
-import { isISBN } from '../utils/confirmISBN';
-
-const displayNotif = (message, color, location) => {
+export const displayNotif = (message, color, location) => {
   const notif = document.createElement('div');
   notif.classList.add('notification');
   notif.classList.add(color);
-  notif.innerHTML(message);
+  notif.innerHTML = message;
 
-  if (location == 'form') {
+  if (location === 'form') {
     const form = document.getElementById('form');
-    form.appendChild(notif);
-  } else if (location == 'table') {
-    const table = document.getElementById('book-table');
-    table.appendChild(notif);
-  }
+    notif.insertAdjacentElement('afterbegin', form);
+    console.log("testing111");
+  } 
+  // else if (location === 'table') {
+  //   const table = document.getElementById('book-table');
+  // }
 
   setTimeout(() => {
     notif.remove();
   }, 5000);
 };
 
-if (isISBN(ISBN) == false) {
-  displayNotif(
-    "Invalid ISBN. Please make sure it's in ISBN-13 format.",
-    'is-danger',
-    'form'
-  );
-}
+export default displayNotif;
